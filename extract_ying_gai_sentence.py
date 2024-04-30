00ae9592a82b3a6591194f47ca785202f7517745
tienchenlee <appielee4305@gmail.com>
2024-04-30 14:23:41 +0800

    我又改了

--- extract_ying_gai_sentence.py
+++ extract_ying_gai_sentence.py
@@ -4,29 +4,31 @@ with open("ying_gai_raw.txt", "r", encoding = "utf-8") as f:
     sentences = re.split(r'more', List)
     #print(sentences)
     
+    # 正則表達式模式
+    pattern = r'[「，。？∥]([^。，∥\n]*?\n應該\t.*?[，。？∥])'
+    
+    # 提取符合模式的句子
+    matching_sentences = []
     for sentence in sentences:
-                                                    pattern = r"[，。？∥、].*\n應該\t.*[，。？∥]"
-                                                    matches = re.findall(pattern, sentence)                                                 
-                                                    #print(matches)
-                                                    extract_sentence = []
-                                                    for match in matches:
-                                                                                                    extract_sentence.append(match[1:])
-                                                                                                    print(extract_sentence)    
+        matches = re.findall(pattern, sentence)
+        matching_sentences.extend(matches)
+    
+    # 輸出結果
+    for match in matching_sentences:
+        print(match)
+        
+    #for sentence in sentences:
+                                                    #pattern = r"[，。？∥、].*\n應該\t.*[，。？∥]"
+                                                    #matches = re.findall(pattern, sentence)                                                 
+                                                    ##print(matches)
+                                                    #extract_sentence = []
+                                                    #for match in matches:
+                                                                                                    #extract_sentence.append(match[1:])
+                                                                                                    #print(extract_sentence)    
                                                     
 
     
-    
-#List = ['共有 5092 筆資料\n\n', '次得獎後能夠引起經紀人的垂青。但他認為楊文信目前不\n應該\t放棄樂團的工作。國內愛樂人十二月十日將可一睹這位青']
 
-#import re
-#for sentence in List:
-                                                #pattern = r"[，。？∥].*\n應該\t.*[，。？∥]"
-                                                #matches = re.findall(pattern, sentence)
-                                                ##print(matches)
-#extract_sentence = []
-#for match in matches:
-                                                #extract_sentence.append(match[1:])
-                                                #print(extract_sentence)
     
         
         
