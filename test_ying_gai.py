017a8654f4d6f089c53198757d617e404baa92d6
ellenyp <ivickie621@gmail.com>
2024-05-01 14:58:35 +0800

    Update test_ying_gai.py

--- test_ying_gai.py
+++ test_ying_gai.py
@@ -3,32 +3,32 @@ with open("ying_gai_raw.txt", "r", encoding = "utf-8") as f:
 resultDICT = {"Sentence: ": ""}
 
 #以more 為關鍵字合併前後句
-    #import re
-    #sentences = re.split(r'more', List)
-    #print(sentences)
+import re
+    sentences = re.split(r'more', List)
+    print(sentences)
 
 
-row = []
-word = "more"
-file = open("new_ying_gai.txt", "w", encoding = "utf-8")
+#row = []
+#word = "more"
+#file = open("new_ying_gai.txt", "w", encoding = "utf-8")
 
-for i, line in enumerate(List, start = 1):
-    #print(line)
-    if word in line:
-        a = i
+#for i, line in enumerate(List, start = 1):
+    ##print(line)
+    #if word in line:
+        #a = i
         
-        row.append(i)
-    else:
-        ()
-    if i in row:
-        print(line)
-        file.write(line)
+        #row.append(i)
+    #else:
+        #()
+    #if i in row:
+        #print(line)
+        #file.write(line)
             
-    file = open("ying_gai_finish.txt", "w", encoding = "utf-8")
-    for sentence in sentences:
-        file.write(sentence)
-        file.write("\n")
-        #print(sentence) 
-file.close()
-f.close()
+    #file = open("ying_gai_finish.txt", "w", encoding = "utf-8")
+    #for sentence in sentences:
+        #file.write(sentence)
+        #file.write("\n")
+        ##print(sentence) 
+#file.close()
+#f.close()
 
